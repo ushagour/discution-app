06918a25d0c2c6f3c81ea4e4daafd02b9f5bb700
ali <ali.ouchagour01@gmail.com>
2021-10-06 01:00:17 +0200

    shere view with app service provider bach ndiir wahd menu f ga3 les page dyall application ila kan user thoenticated

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\Channel;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +27,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+
+        View::share('channels',Channel::all());
+
+
     }
 }
