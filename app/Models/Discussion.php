b69a5d784c803193cdcf4373e3c8ca8266aee32a
ali <ali.ouchagour01@gmail.com>
2021-10-15 20:23:48 +0200

    fixing some mistakes in model

--- app/Models/Discussion.php
+++ app/Models/Discussion.php
@@ -2,10 +2,11 @@
 
 namespace App\Models;
 
+use App\Models\Model as ModelsModel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Discussion extends Model
+class Discussion extends ModelsModel
 {
     use HasFactory;
    // To do we will use global model  later 
--- app/Models/Model.php
+++ app/Models/Model.php
@@ -1,10 +1,10 @@
 <?php
-namespace App;
-use Illuminate\Database\Eloquent\Model;
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model as BaseModel;
 
 
 class Model extends BaseModel
 {
-    protected $guarded=[];  
+    protected $guarded = [];  
     
 }
