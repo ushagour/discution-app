4abc55662db8c5358eef6bc1e9413ae8258ab217
ali <ali.ouchagour01@gmail.com>
2021-10-26 20:39:05 +0200

    creating replay migration and editing content discussion type

--- database/migrations/2021_10_23_172049_create_replies_table.php
+++ database/migrations/2021_10_23_172049_create_replies_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateDiscussionsTable extends Migration
+class CreateRepliesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,11 @@ class CreateDiscussionsTable extends Migration
      */
     public function up()
     {
-        Schema::create('discussions', function (Blueprint $table) {
+        Schema::create('replies', function (Blueprint $table) {
             $table->id();
-            $table->string("title");
-            $table->string("slug");
-            $table->text("content");
-            $table->integer("channel_id");
-            $table->integer("user_id");
+            $table->integer('user_id');
+            $table->integer('discussion_id');
+            $table->text('content');
             $table->timestamps();
         });
     }
@@ -31,6 +29,6 @@ class CreateDiscussionsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('discussions');
+        Schema::dropIfExists('replies');
     }
 }
