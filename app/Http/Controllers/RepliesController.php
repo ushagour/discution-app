78eddb89a1bb7eccd93c8447397eef7607128ea2
ali <ali.ouchagour01@gmail.com>
2021-10-26 20:40:31 +0200

    reply controller and model  withe request

---
+++ app/Http/Controllers/RepliesController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CreateReplyRequest;
+use App\Models\Discussion;
+use Illuminate\Http\Request;
+
+class RepliesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param   instance dyal object Discussion li ghadii ndiro lih replay
+     * @return \Illuminate\Http\Response
+     */
+    public function store(CreateReplyRequest $request, Discussion $discussion)
+    {
+        //had l kitaba below khassni nchoofha 
+        auth()->user()->replies()->create([
+            'content'=>$request->content,
+            'discussion'=>$discussion->id
+
+        ]);
+        
+      return  redirect()->back();
+        
+    
+
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
