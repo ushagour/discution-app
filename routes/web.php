91687bc0441c771258a1a6d88cdf4ce0cd466026
ali <ali.ouchagour01@gmail.com>
2021-11-06 17:26:29 +0100

    craating controller with function notification to display them and make them read

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\DiscussionController;
 use App\Http\Controllers\RepliesController;
+use App\Http\Controllers\UsersController;
 use App\Models\Discussion;
 use Illuminate\Support\Facades\Route;
 
@@ -19,12 +20,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Auth::routes();
 Route::resource('discussions',DiscussionController::class);
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
+Route::get('users/notifications',[UsersController::class,'notifications']);//had route dyal notification bach y9rahoom 
 Route::resource('discussions/{discussion}/replies',RepliesController::class); //todo hta ntell 3la hadii kiifach katkhdm 
 
 Route::POST('discussions/{discussion}/replies/{reply}',[DiscussionController::class,'BestReply'])->name('discussion.best-reply');
+
