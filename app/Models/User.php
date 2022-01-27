0f42a285ec2ee932d800d79d529dc0791a06e27e
ali <ali.ouchagour01@gmail.com>
2022-01-28 00:50:28 +0100

    creating settings model and profile with database migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,5 +51,11 @@ class User extends Authenticatable implements MustVerifyEmail
     {
          return   $this->hasMany(Like::class);
     }
+
+    public function profile()
+    {
+         return   $this->hasOne(Profile::class);
+    }
  
+
 }
