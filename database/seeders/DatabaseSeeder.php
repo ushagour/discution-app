b9010e74b3915468a1e5a5fce08582b7ca296a90
ali <ali.ouchagour01@gmail.com>
2022-02-13 14:57:43 +0100

    fixing erors in db seeder to finishing tests sucsuufuly

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Discussion;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            UserSeeder::class,
+            ChannelSeeder::class
+        
+        ]);
+        User::factory(3)->create();
+        Discussion::factory(3)->create();
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use App\Models\User;
+
 
 
 class UserSeeder extends Seeder
@@ -18,19 +20,12 @@ class UserSeeder extends Seeder
     public function run()
     {
         //
-        $user=  App\User::create([ 
+        $user=  User::create([ 
             'name' => 'admin',
-            'username' => 'admin',
             'email' => 'admin@gmail.com',
             'password' => Hash::make('password'), //remember! the password is passowrd by defult 
             'is_admin'  =>1
           ]);
-        $user=  App\User::create([ 
-            'name' => 'user',
-            'username' => 'user',
-            'email' => 'user@gmail.com',
-            'password' => Hash::make('password'), //remember! the password is passowrd by defult 
-            'is_admin'  =>0
-          ]);
+
     }
 }
